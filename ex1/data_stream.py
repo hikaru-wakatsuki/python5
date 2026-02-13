8cadef63fbcd10d66ce8f1786fac7343b5b619d4
若月　洸 <hikaru@wakatsukikounoMacBook-Air.local>
2026-02-13 16:45:30 +0900

    waka

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -41,59 +41,92 @@ class SensorStream(DataStream):
             filtered += self.filter_data(data_batch, "temp")
             filtered += self.filter_data(data_batch, "humidity")
             filtered += self.filter_data(data_batch, "pressure")
-            data: Dict[str, Union[int, float]] = create_dict(filtered)
-            if data == {}:
-                raise Exception
             sum_temp: Union[int, float] = 0
             count_temp: int = 0
-            for key, value in data.items():
+            for line in filtered:
+                if not isinstance(line, str):
+                    raise TypeError()
+                words: List[str] = line.split(":")
+                i: int = 0
+                for _ in words:
+                    i += 1
+                if i != 2:
+                    raise ValueError("Invalid data format")
+                key: str = words[0]
+                value: Union[int, float] = float(words[1])
                 if key == "temp" and -20 <= value <= 50:
-                    total_items += 1
+                    self.total_items += 1
                     count_temp += 1
                     sum_temp += value
                 if key == "humidity" and 0 <= value <= 100:
-                    total_items += 1
+                    self.total_items += 1
                 if key == "pressure" and 900 <= value <= 1050:
-                    total_items += 1
+                    self.total_items += 1
             self.processed_batches += 1
             if count_temp == 0:
                 raise Exception
             avg_temp: Union[int, float] = sum_temp / count_temp
-            return (f"Sensor analysis: {total_items} readings processed, avg temp: {avg_temp}°C")
+            return f"{self.total_items} readings processed, avg temp: {avg_temp}°C"
         except Exception as e:
-            return e
-
+            return f"{e}"
 
 class TransactionStream(DataStream):
     def __init__(self, stream_id: str):
         super().__init__(stream_id, "Financial Data")
 
+    def process_batch(self, data_batch: List[Any]) -> str:
+        try:
+            filtered: List[Any] = []
+            filtered += self.filter_data(data_batch, "buy")
+            filtered += self.filter_data(data_batch, "sell")
+            sum: int = 0
+            for line in filtered:
+                if not isinstance(line, str):
+                    raise TypeError()
+                words: List[str] = line.split(":")
+                i: int = 0
+                for _ in words:
+                    i += 1
+                if i != 2:
+                    raise ValueError("Invalid data format")
+                key: str = words[0]
+                value: int = int(words[1])
+                if key == "buy" and  value > 0:
+                    self.total_items += 1
+                    sum += value
+                if key == "sell" and value > 0:
+                    self.total_items += 1
+                    sum -= value
+            self.processed_batches += 1
+            return f"{self.total_items} operations, net flow: {sum:+} units"
+        except Exception as e:
+            print(e)
+            return f"{e}"
 
 class EventStream(DataStream):
     def __init__(self, stream_id: str):
         super().__init__(stream_id, "System Events")
 
+    def process_batch(self, data_batch: List[Any]) -> str:
+        try:
+            data: List[Any] = []
+            data += self.filter_data(data_batch)
+            total_count: int = 0
+            error_count: int = 0
+            for event in data:
+                if event == "error":
+                    self.total_items += 1
+                    total_count += 1
+                    error_count += 1
+                else:
+                    total_count += 1
+            self.processed_batches += 1
+            return f"{total_count} events, {error_count} error detected"
+        except Exception as e:
+            print(e)
+            return f"{e}"
 
 
-
-def create_dict(data_batch: List[Any]) -> Dict[str, Union[int, float]]:
-    try:
-        data: Dict[str, Union[int, float]] = {}
-        for line in data_batch:
-            if not isinstance(line, str):
-                raise TypeError()
-            words: list[str] = line.split(":")
-            i: int = 0
-            for _ in words:
-                i += 1
-            if i != 2:
-                raise ValueError("Invalid data format")
-            data[words[0]] = float(words[1])
-        return data
-
-    except Exception as e:
-        print(e)
-        return {}
 
 
 
